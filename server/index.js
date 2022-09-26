721552a2961771c5ddfeccfecccdb24566814da7
Miles Wiesenthal <miles@Miless-MacBook-Pro.local>
2022-09-25 17:41:33 -0700

    different ssl

--- server/index.js
+++ server/index.js
@@ -12,11 +12,10 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https')
       res.redirect(`https://${req.header('host')}${req.url}`)
     else
